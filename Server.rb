abc05a03d08333a58cfee94eab59c2c847e9f8c1
Omnigrass <harpreethighlyreactive@gmail.com>
2015-11-23 20:19:39 +0000

    Added hash for clientName and chatRooms and made the code more modular

--- Server.rb
+++ Server.rb
@@ -9,95 +9,111 @@ class Server
     @host=host
     @port=port
     @descriptors=Array.new #Stores all client sockets and the server socket
-    @clientName=Array.new #Probably this isn't of any use either
-    @clientNameCurrent #This is of no use
+    @clientName=Hash.new #Probably this isn't of any use either
     @serverSocket= TCPServer.open(@host,@port)
     @descriptors.push(@serverSocket)
     @threadPool=Thread.pool(4) #There can be a maximum of 4 threads at a time
     @retryJoinReqFlag=0
-    @chatroom=Hash.new
-    @sock_domain 
-    @remote_port 
-    @remote_hostname 
-    @remote_ip
+    @chatRoom=Hash.new
     @StudentID=ARGV[2]||152
   end
   
   def welcome(client)
-    @sock_domain, @remote_port, @remote_hostname, @remote_ip = client.peeraddr #this is of no use either
-    puts "log: Connection from #{@remote_ip} at port #{@remote_port}"
+    puts "log: Connection from client #{client}"
   end
   
   def chatRoom(chatname)
+    flag=0
+    @chatRoom.each do |key, value|
+      if key==hash
+         #chat=value
+         flag=1
+      end
+    end
+    if flag==0
+    newChatroom=hashCode(chatname)
+    @chatRoom[newChatroom]=chatname
+    puts "log:chatroom #{chatname} created"
+    puts "log:printing @chatRoom #{@chatRoom}"
+    end
+  end
+
+  def hashCode(str)
     hash=0
-    chatname.each_byte do |i| 
+    str.each_byte do |i| 
       hash=hash*31 + i 
     end
-    puts "log:chatroom #{chatname} created"
     return hash     
   end
   
-
+  def initiateCheckname(username, client)
+    reply=checkname(username, client)
+    if reply==1 # If the username is already taken, get response from client if it wants to retry or not
+      input=client.gets
+          if input[0,5]=="Close" # If the answer recieved is close, then close the connection
+            puts "log: Closing connection to client on port #{@remote_port}"
+            @descriptors.pop(client)
+            client.close
+          else    #The client wants to retry
+            @retryJoinReqFlag=1
+          end
+    end
+  end
+  
+  def sendJoinReqMsg(join_details, client)
+    chatRoom(join_details[0])
+    client.puts "JOINED_CHATROOM:#{join_details[0]}\nSERVER_IP:#{@host}\nPORT:#{@port}\nROOM_REF:#{@chatRoom.key(join_details[0])}\nJOIN_ID:#{@clientName.key(join_details[3])}\n"
+  end
+  
   def servJoinReq(input,client)
     join_details=Array.new 
     join_details[0]=input.slice((input.index(':')+1)..input.length)   
     i=1
-    while (i<=3)
-        input=client.gets.chomp
-        join_details[i]=input.slice((input.index(':')+1)..input.length)
-        if i==3
-            flag=checkname(join_details[3], client)
-            if flag==1 # If the username is already taken, get response from client if it wants to retry or not
-              input=client.gets
-              if input[0,5]=="Close" # If the answer recieved is close, then close the connection
-                puts "log: Closing connection to client on port #{@remote_port}"
-                @descriptors.pop(client)
-                client.close
-              else    #The client wants to retry, come out of the function servJoinReq
-                @retryJoinReqFlag=1
-                break
-              end
-            end
-        end
-        i+=1
-    end
-    
-      if @retryJoinReqFlag==0
-        room_ref=chatRoom(join_details[0])
-        client.puts "JOINED_CHATROOM:#{join_details[0]}\nSERVER_IP:#{@host}\nPORT:#{@port}\nROOM_REF:#{room_ref}\nJOIN_ID: 1\n"
-      else 
-        puts "log: going back to run"
+      while (i<=3)
+          input=client.gets.chomp
+          join_details[i]=input.slice((input.index(':')+1)..input.length)
+          if i==3
+           initiateCheckname(join_details[3], client)  
+          end
+          if @retryJoinReqFlag==1 # If the client opts to get a new username, come of the loop and go back to the run method
+            break
+          end
+          i+=1
       end
+      
+        if @retryJoinReqFlag==0
+          sendJoinReqMsg(join_details, client)
+        end
   end
 
   def raiseError(id, client)
     if id==0
-      client.puts "ERROR_CODE:#{id}\nERROR_DESCRIPTION: Username already taken!! "
+      client.puts "ERROR_CODE:#{id}\nERROR_DESCRIPTION:Username already taken!! "
     end
   end
 
-  
   def checkname(input, client) #checks if username is available, returns 1 if it's already taken, 0 if it's available
     flag=0
-      @clientName.each do |name| #If username is already taken, flag is set to 1
+      @clientName.each do |key, name| #If username is already taken, flag is set to 1
         if name==input              
           flag=1
         end
       end
-      if flag==0  #if username is available, it is pushed into the array @clientName
-        @clientName.push(input)
+      if flag==0  #if username is available, it is pushed into the hash @chatName
+        clientID=hashCode(input)
+        @clientName[clientID]=input
         puts "log: Username #{input} created for client on port #{@remote_port}"
       else
         raiseError(0, client)
       end
+      puts "log: printing @clientName in checkname method#{@clientName}"
       return flag
   end
 
   def new_Connection(client)
       while true
       input=client.gets.chomp
-      puts "log: got input from client #{@remote_port}"
-      puts "From #{client} #{@remote_port}: #{input}"
+      puts "log: From #{client}: #{input}"
       handle_Connection(input, client)
       end
   end
@@ -110,7 +126,7 @@ class Server
       terminate
     elsif input[0,13]=="JOIN_CHATROOM"
       servJoinReq(input, client)
-      if @retryJoinReqFlag==1
+      if @retryJoinReqFlag==1 #check if the username already taken error was raised and the client opted to try again
          @retryJoinReqFlag=0
          servJoinReq(input, client)
       end
